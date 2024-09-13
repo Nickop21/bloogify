99f27db478379c535ab1890e913aed545088e1e5
Nickop21 <ny8244@gmail.com>
2024-09-13 19:32:30 +0530

    home page static section + clerk installataion

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import Banner from "@/components/home/banner";
+import HowItWorks from "@/components/home/howItWorks";
+import Pricing from "@/components/home/pricing";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
 <>
-<Button variant={"destructive"}>
-  dhsb
-</Button>
+<Banner/>
+<HowItWorks/>
+<Pricing/>
 </>
   );
 }
---
+++ components/home/header.tsx
@@ -0,0 +1,50 @@
+// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+const NavLink = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Link
+      href={href}
+      className="transition-colors duration-200 text-gray-600 hover:text-purple-500"
+    >
+      {children}
+    </Link>
+  );
+};
+function Header() {
+  return (
+    <nav className="container flex items-center justify-between px-8 py-4 mx-auto">
+      <div className="flex lg:flex-1">
+        <NavLink href="/">
+          <span className="flex items-center gap-2 shrink-0">
+            <Image
+              src="/icon.ico"
+              alt="SpeakEasy logo"
+              width={32}
+              height={32}
+              className="hover:rotate-12 transform transition duration-200 ease-in-out"
+            />
+            <span className="font-extrabold text-lg">SpeakEasy</span>
+          </span>
+        </NavLink>
+      </div>
+      <div className="flex lg:justify-center gap-2 lg:gap-12 lg:items-center">
+        <NavLink href="/#pricing">Pricing</NavLink>
+      
+      </div>
+
+      <div className="flex lg:justify-end lg:flex-1">
+      
+      </div>
+    </nav>
+  )
+}
+
+export default Header
